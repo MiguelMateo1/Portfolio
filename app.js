e1c178fb932288619022c103197bc23b1b41dea3
miguel mateo <mmateo95@hotmail.com>
2023-08-22 15:01:12 -0400

    bg img transition added + fade animation added

--- app.js
+++ app.js
@@ -143,3 +143,67 @@ sliderWrapper.addEventListener("mouseleave", () => {
   startSlideShow();
 });
 //  certificats container END====
+
+
+//== fade in anaimation (projects)
+function fadeInElement(element) {
+  element.classList.add('fade-in');
+}
+
+const projectContainers = document.querySelectorAll('.project-container');
+
+// display the fisrt ele in projectsContainer on page load
+const observer = new IntersectionObserver(entries => {
+  if (entries[0].isIntersecting) {
+      fadeInElement(entries[0].target);
+      observer.unobserve(entries[0].target);
+  }
+});
+observer.observe(projectContainers[0]);
+
+
+const isElementInViewport = element => {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top <= window.innerHeight - 200 &&
+    rect.bottom >= 0 &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+// adds classlist on scroll
+const fadeInOnScroll = () => {
+  projectContainers.forEach(projectContainer => {
+      if (projectContainer !== projectContainers[0] && isElementInViewport(projectContainer)) {
+          fadeInElement(projectContainer);
+      }
+  });
+};
+
+window.addEventListener('scroll', fadeInOnScroll);
+fadeInOnScroll();
+// fade in anaimation END ==
+
+
+//== bg image transition effect
+const bgImgElements = document.querySelectorAll('.bg-img');
+let lastScrollY = window.scrollY;
+const imgWidth = 900;
+const initialOpacity = 0.7;
+
+function handleScroll() {
+  const currentScrollY = window.scrollY;
+
+  bgImgElements.forEach(bgImg => {
+    // Calculate new width and opacity based on scroll
+    const newWidth = imgWidth + currentScrollY;
+    const newOpacity = Math.max(initialOpacity - currentScrollY / 800, 0);
+
+    // Apply new width and opacity to the styles of the current element
+    bgImg.style.width = `${newWidth}px`;
+    bgImg.style.opacity = newOpacity.toFixed(2); // Limit opacity to 2 decimal places
+  });
+}
+
+window.addEventListener('scroll', handleScroll);
+// bg image transition effect END==
